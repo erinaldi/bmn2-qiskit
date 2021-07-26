5850baf670c87105b9af38a2e78bacf00f6ba7ae
Enrico Rinaldi <erinaldi.work@gmail.com>
2021-07-26 17:16:55 +0900

    [fix] formatted script

--- scripts/02_bmn2_mini_VQE.py
+++ scripts/02_bmn2_mini_VQE.py
@@ -1,7 +1,9 @@
 # %%
-import numpy as np
 import time
-from matplotlib import pyplot as plt 
+import fire
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
 from scipy.sparse import diags
 from scipy.sparse import identity
 from scipy.sparse import kron
@@ -26,55 +28,65 @@ def bmn2_hamiltonian(L: int = 2, N: int = 2, g2N: float = 0.2):
     """
     L = 2  # cutoff for Fock space -> can not go larger than 2 in qiskit without having problems for miniBMN
     # The annihilation operator for the single boson
-    a_b = diags(np.sqrt(np.linspace(1,L-1,L-1)),offsets=1)
+    a_b = diags(np.sqrt(np.linspace(1, L - 1, L - 1)), offsets=1)
     # The identity operator of the Fock space of a single boson
     i_b = identity(L)
     # The annihilation operator for the fermions (always cutoff = 2 because it is a spin)
-    a_f = diags(np.sqrt(np.linspace(1,1,1)),offsets=1)
+    a_f = diags(np.sqrt(np.linspace(1, 1, 1)), offsets=1)
     # The Pauli $\sigma_z$ matrix
-    sz = diags([1.0,-1.0])
+    sz = diags([1.0, -1.0])
     # Identity for the single fermion space
     i_f = identity(2)
     # Bosonic Hilbert space
-    N_bos = int(2*(N**2-1))  # number of boson sites -> fixed for mini-BMN 2
+    N_bos = int(2 * (N ** 2 - 1))  # number of boson sites -> fixed for mini-BMN 2
     product_list = [i_b] * N_bos  # only the identity for bosons repeated N_bos times
     a_b_list = []  # this will contain a1...a6
-    for i in np.arange(0,N_bos):  # loop over all bosonic operators
+    for i in np.arange(0, N_bos):  # loop over all bosonic operators
         operator_list = product_list.copy()  # all elements are the identity operator
-        operator_list[i] = a_b  # the i^th element is now the annihilation operator for a single boson
-        a_b_list.append(operator_list[0]) # start taking tensor products from first element
+        operator_list[
+            i
+        ] = a_b  # the i^th element is now the annihilation operator for a single boson
+        a_b_list.append(
+            operator_list[0]
+        )  # start taking tensor products from first element
         for a in operator_list[1:]:
-            a_b_list[i] = kron(a_b_list[i],a)  # do the outer product between each operator_list element
+            a_b_list[i] = kron(
+                a_b_list[i], a
+            )  # do the outer product between each operator_list element
     # Fermionic Hilbert space
-    N_f = 3  # number of fermionic sites -> fixed for the supersymmetric model mini-BMN with 2 matrices 
+    N_f = 3  # number of fermionic sites -> fixed for the supersymmetric model mini-BMN with 2 matrices
     product_list = [i_f] * N_f  # only the identity for fermions repeated N_f times
     a_f_list = []  # this will contain f1...f3
-    for i in np.arange(0,N_f):  # loop over all bosonic operators
+    for i in np.arange(0, N_f):  # loop over all bosonic operators
         operator_list = product_list.copy()  # all elements are the identity operator
-        operator_list[i] = a_f  # the i^th element is now the annihilation operator for a single fermion
-        for j in np.arange(0,i):  # the 0:(i-1) elements are replaced by sigma_Z
+        operator_list[
+            i
+        ] = a_f  # the i^th element is now the annihilation operator for a single fermion
+        for j in np.arange(0, i):  # the 0:(i-1) elements are replaced by sigma_Z
             operator_list[j] = sz
-        a_f_list.append(operator_list[0])  # start taking tensor products from the first operator
+        a_f_list.append(
+            operator_list[0]
+        )  # start taking tensor products from the first operator
         for a in operator_list[1:]:
-            a_f_list[i] = kron(a_f_list[i],a)  # do the outer product 
+            a_f_list[i] = kron(a_f_list[i], a)  # do the outer product
     # Combine the Bosonic and Fermionic space
     # - Identity for bosonic space (dimension will be $L^{N_{bos}} \times L^{N_{bos}}$)
-    i_b_tot = identity(L**N_bos) 
+    i_b_tot = identity(L ** N_bos)
     # - Identity for fermionic space (dimension will be $2^{N_f} \times 2^{N_f}$)
-    i_f_tot = identity(2**N_f)
+    i_f_tot = identity(2 ** N_f)
     # The new bosonic and fermionic operators combined into one list
     op_list = []
     for a in a_b_list:
-        op_list.append(kron(a,i_f_tot))
+        op_list.append(kron(a, i_f_tot))
     for a in a_f_list:
-        op_list.append(kron(i_b_tot,a))
+        op_list.append(kron(i_b_tot, a))
     # Build the Hamiltonian
     # Start piece by piece
     x_list = []
     # only use the bosonic operators
     bosons = op_list[:N_bos]
     for op in bosons:
-        x_list.append(1/np.sqrt(2)*(op.conjugate().transpose() + op))
+        x_list.append(1 / np.sqrt(2) * (op.conjugate().transpose() + op))
     # save the fermions operators for convenience in a new list
     fermions = op_list[-N_f:]
     # Free Hamiltonian
@@ -114,7 +126,7 @@ def bmn2_hamiltonian(L: int = 2, N: int = 2, g2N: float = 0.2):
         - (x_list[2] + 1j * x_list[5]) * fermions[1] * fermions[0]
     )
     # full hamiltonian
-    return H_k + g2N/N*V_b + np.sqrt(g2N/N)*V_bf
+    return H_k + g2N / N * V_b + np.sqrt(g2N / N) * V_bf
 
 
 def eigenvalues_scipy(H, k: int = 10):
@@ -124,7 +136,7 @@ def eigenvalues_scipy(H, k: int = 10):
         H (scipy.sparse matrix): The Hamiltonian in the form of a sparse matrix
         k (int): The number of lowest eigenvalues to compute. Defaults to 10.
     """
-    eigv = eigsh(H,k,which='SA',return_eigenvectors=False,tol=0)
+    eigv = eigsh(H, k, which="SA", return_eigenvectors=False, tol=0)
     return np.real(eigv[::-1])
 
 
@@ -140,8 +152,17 @@ def eigenvalues_qiskit(qOp: MatrixOp, k: int = 10):
     result = mes.compute_eigenvalues(qOp)
     return np.real(result.eigenvalues)
 
+
 # %%
-def main(L: int = 2, N: int = 2, g2N: float = 0.2, optimizer: str = "COBYLA", varform: list = ['ry'], depth: int = 3, nrep: int = 1):
+def run_vqe(
+    L: int = 2,
+    N: int = 2,
+    g2N: float = 0.2,
+    optimizer: str = "COBYLA",
+    varform: list = ["ry"],
+    depth: int = 3,
+    nrep: int = 1,
+):
     """Run the main VQE solver for a minimal BMN Hamiltonian where bosons are LxL matrices and the 't Hooft coupling is g2N for a SU(N) gauge group.
     The VQE is initialized with a specific optimizer and a specific variational quantum circuit based on EfficientSU2.
 
@@ -160,24 +181,31 @@ def main(L: int = 2, N: int = 2, g2N: float = 0.2, optimizer: str = "COBYLA", va
     qubitOp = MatrixOp(primitive=H)
     # check the exact eigenvalues
     print(f"Exact Result of discrete hamiltonian (matrix): {eigenvalues_scipy(H)}")
-    print(f"Exact Result of discrete hamiltonian (qubit): {eigenvalues_qiskit(qubitOp)}")
+    print(
+        f"Exact Result of discrete hamiltonian (qubit): {eigenvalues_qiskit(qubitOp)}"
+    )
 
     # Next, we create the variational form.
-    var_form = EfficientSU2(qubitOp.num_qubits, su2_gates=varform, entanglement="full", reps=depth)
+    var_form = EfficientSU2(
+        qubitOp.num_qubits, su2_gates=varform, entanglement="full", reps=depth
+    )
 
     # start a quantum instance
     # fix the random seed of the simulator to make values reproducible
     seed = 50
     algorithm_globals.random_seed = seed
-    backend = Aer.get_backend("statevector_simulator", max_parallel_threads=6, max_parallel_experiments=0)  
+    backend = Aer.get_backend(
+        "statevector_simulator", max_parallel_threads=6, max_parallel_experiments=0
+    )
     q_instance = QuantumInstance(backend, seed_transpiler=seed, seed_simulator=seed)
 
     # initialize optimizers' parameters: number of iterations
-    optimizers = {"COBYLA": COBYLA(maxiter=5000),
-                "L-BFGS-B": L_BFGS_B(maxiter=5000),
-                "SLSQP": SLSQP(maxiter=5000),
-                "NELDER-MEAD": NELDER_MEAD(maxiter=5000)
-                }
+    optimizers = {
+        "COBYLA": COBYLA(maxiter=5000),
+        "L-BFGS-B": L_BFGS_B(maxiter=5000),
+        "SLSQP": SLSQP(maxiter=5000),
+        "NELDER-MEAD": NELDER_MEAD(maxiter=5000),
+    }
 
     start_time = time.time()
     optim = optimizers[optimizer]
@@ -196,7 +224,13 @@ def main(L: int = 2, N: int = 2, g2N: float = 0.2, optimizer: str = "COBYLA", va
         # initital points
         random_init = np.random.random(var_form.num_parameters)
         # Setup the VQE algorithm
-        vqe = VQE(ansatz=var_form, optimizer=optim, initial_point=random_init, quantum_instance=q_instance, callback=store_intermediate_result)
+        vqe = VQE(
+            ansatz=var_form,
+            optimizer=optim,
+            initial_point=random_init,
+            quantum_instance=q_instance,
+            callback=store_intermediate_result,
+        )
         # run the VQE with out Hamiltonian operator
         result = vqe.compute_minimum_eigenvalue(qubitOp)
         vqe_result = np.real(result.eigenvalue)
@@ -205,9 +239,16 @@ def main(L: int = 2, N: int = 2, g2N: float = 0.2, optimizer: str = "COBYLA", va
         results["energy"].append(values)
 
     end_time = time.time()
-    runtime = end_time-start_time
+    runtime = end_time - start_time
     print(f"Program runtime: {runtime} s")
-    outfile = f"data/{optimizer}_{"-".join(varform)}_{nrep}.npy"
+    # make a dataframe from the results and save it on disk with HDF5
+    df = pd.DataFrame.from_dict(results)
+    varname = "-".join(varform)
+    outfile = f"data/{optimizer}_{varname}_{nrep}.npy"
     print(f"Save results on disk: {outfile}")
-    np.save(outfile,)
+    df.to_hdf(outfile, "vqe")
 
+
+# %%
+if __name__ == "__main__":
+    fire.Fire(run_vqe)
